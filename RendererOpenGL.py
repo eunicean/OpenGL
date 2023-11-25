df672188f9e3c7b0344409ac28be7d050fc5d175
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-11-24 23:39:30 -0600

    models, music and movement added

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from gl import Renderer
 from model import Model
 from shaders import *
+from OpenGL.GL import glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
 
 width = 960
 height = 540
@@ -13,40 +14,34 @@ pygame.init()
 screen = pygame.display.set_mode((width,height), pygame.OPENGL | pygame.DOUBLEBUF)
 clock = pygame.time.Clock()
 
-rend = Renderer(screen)
+pygame.mixer.music.load("aot.mp3")
+pygame.mixer.music.play(-1)
 
+print("")
+
+rend = Renderer(screen)
 
 rend.setShader(vertex_shader, fragment_shader)
 
+model = [
+    rend.loadModel(filename="modelos/PenguinBaseMesh.obj",texture="texturas/PenguinDiffuseColor.bmp"),
+    rend.loadModel(filename="modelos/BearSaddle.obj",texture="texturas/MaterialLightBrown.png", rotation=(0,180,0)),
+    rend.loadModel(filename="modelos/Bread.obj",texture="texturas/Bread.jpg", position=(0,0,-5), scale=(15,15,15)),
+    rend.loadModel(filename="modelos/Viking Sword.obj",texture = "texturas/VikingSword.png", position=(0,0,-5),rotation=(0,90,0), scale=(5,5,5))
+]
+active_model_index = 0
 
-# #           POSITION        #COLOR
-# triangleData = [-0.5, -0.5, 0,  1.0, 0.0, 0.0,
-#                    0,  0.5, 0,  0.0, 1.0, 0.0,
-#                  0.5, -0.5, 0,  0.0, 0.0, 1.0]
+radius = 5.0
+angle = 0.0
 
-# #               POSITIOn        UVs
-# triangleData = [-0.5, -0.5, 0,  0.0, 0.0,   0.0,0.0,1.0,
-#                 -0.5,  0.5, 0,  0.0, 1.0,   0.0,0.0,1.0,
-#                  0.5, -0.5, 0,  1.0, 0.0,   0.0,0.0,1.0,
-                 
-#                 -0.5,  0.5, 0,  0.0, 1.0,   0.0,0.0,1.0,
-#                  0.5,  0.5, 0,  1.0, 1.0,   0.0,0.0,1.0,
-#                  0.5, -0.5, 0,  1.0, 0.0,   0.0,0.0,1.0]
-
-# triangleModel = Model(triangleData)
-# triangleModel.loadTexture("cea.jpg")
-# triangleModel.position.z = -5
-# triangleModel.scale = glm.vec3(3,3,3)
-
-# rend.scene.append(triangleModel)
-
-penguin = rend.loadModel(filename="PenguinBaseMesh.obj",texture="PenguinDiffuseColor.bmp")
-rend.scene.append(penguin)
+rend.scene.append(model[active_model_index])
 
 isRunning = True
+zoom_factor = 1.0
 while isRunning:
     deltaTime = clock.tick(60) / 1000
     keys = pygame.key.get_pressed()
+
 
     for evente in pygame.event.get():
         if evente.type == pygame.QUIT:
@@ -54,13 +49,43 @@ while isRunning:
         elif evente.type == pygame.KEYDOWN:
             if evente.type == pygame.K_ESCAPE:
                 isRunning = False
+            if evente.key == pygame.K_1:
+                active_model_index = 0
+            if evente.key == pygame.K_2:
+                active_model_index = 1
+            if evente.key == pygame.K_3:
+                active_model_index = 2
+            if evente.key == pygame.K_4:
+                active_model_index = 3
 
+        elif evente.type == pygame.MOUSEBUTTONDOWN:
+            if evente.button == 4: #rodar hacia arriba
+                if rend.camPosition.z < 5.0:
+                    rend.camPosition.z += 0.5
+            if evente.button == 5: #rodar para abajo
+                if rend.camPosition.z > -4.5:
+                    rend.camPosition.z -= 0.5
 
+    active_model = model[active_model_index]
+    rend.scene = [active_model]
+    
     vel = 9
     if keys[K_d]:
-        rend.camPosition.x += vel * deltaTime
-    if keys[K_a]:
-        rend.camPosition.x -= vel * deltaTime
+        angle += 0.5 * deltaTime
+    elif keys[K_a]:
+        angle -= 0.5 * deltaTime
+
+    cam_radius = radius
+    cam_height = 2.0
+    rend.camPosition.x = active_model.position.x + cam_radius * glm.cos(angle)
+    rend.camPosition.y = active_model.position.y + cam_radius * glm.sin(angle)
+    rend.camPosition.z = active_model.position.z + cam_height
+    rend.camTarget = active_model.position
+    
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    
     if keys[K_w]:
         rend.camPosition.z += vel * deltaTime
     if keys[K_s]:
@@ -70,24 +95,20 @@ while isRunning:
     if keys[K_e]:
          rend.camPosition.y -= vel * deltaTime
     if keys[K_z]:
-        penguin.rotation.y += 70 * deltaTime
+        active_model.rotation.y += 70 * deltaTime
     if keys[K_x]:
-        penguin.rotation.y -= 70 * deltaTime
-    if keys[K_1]:
+        active_model.rotation.y -= 70 * deltaTime
+    if keys[K_5]:
         rend.setShader(vertex_shader, fragment_shader)
-    if keys[K_2]:
+    if keys[K_6]:
         rend.setShader(vertex_shader, noise_fragment_shader)
-    if keys[K_3]:
+    if keys[K_7]:
         rend.setShader(ballon_vertex_shader, ballon_fragment_shader)
-    if keys[K_4]:
+    if keys[K_8]:
         rend.setShader(vertex_shader, color_fragment_shader)
 
-    penguin.rotation.y += 45 * deltaTime
-
-    rend.elapsedTime += deltaTime       
-
+    rend.elapsedTime += deltaTime
     rend.render()
     pygame.display.flip()
 
 pygame.quit()
-#31:40 min vid 26sep
